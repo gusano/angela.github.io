f28f52e9b8ef29256ba47266d2488dbacdd963b5
gusano <gusano@users.noreply.github.com>
2024-02-17 22:02:21 +0000

    deploy: 4529786291f5a65f496bf10cdceb72ade25234bd

--- assets/js/carousel.js
+++ assets/js/carousel.js
@@ -1,8 +1,11 @@
 const carousel = document.querySelector(".carousel");
-// const backgroundImage = document.querySelector(".bg-image");
 
 const leftArrow = document.querySelector("#left-arrow");
 const rightArrow = document.querySelector("#right-arrow");
+
+// Swipe
+let touchstartX = 0;
+let touchendX = 0;
 
 let gap = 18;
 let currentIndex = 0;
@@ -11,61 +14,88 @@ const images = document.querySelectorAll(".carousel-image");
 
 const totalImages = Object.keys(images).length;
 
-// Use this in your project, if you're doing it locally
-// const imageWidth = images[1].getBoundingClientRect().x;
-
 const imageWidth = 580;
 
+// Swipe left
+function swipeLeft() {
+  prevIndex = currentIndex;
+  currentIndex = (currentIndex - 1 + totalImages) % totalImages;
+
+  // Use previous image width
+  const index = prevIndex === 0 ? (images.length - 1) : (prevIndex - 1)
+  const w = images[index].getBoundingClientRect().width + gap;
+  // carousel.style.transform = `translateX(-${imageWidth}px)`;
+  carousel.style.transform = `translateX(-${w}px)`;
+  carousel.insertBefore(images[currentIndex], carousel.firstChild);
+
+  setTimeout(() => {
+    carousel.style.transform = "";
+    carousel.classList.add("sliding-transition");
+  }, 10);
+
+  setTimeout(() => {
+    carousel.classList.remove("sliding-transition");
+    done = true;
+  }, 490);
+}
+
+// Swipe right
+function swipeRight() {
+  carousel.classList.add("sliding-transition");
+
+  prevIndex = currentIndex;
+  currentIndex = (currentIndex + 1) % totalImages;
+
+  // Use current image width
+  const w = images[prevIndex].getBoundingClientRect().width + gap;
+
+  // carousel.style.transform = `translateX(-${imageWidth}px)`;
+  carousel.style.transform = `translateX(-${w}px)`;
+
+  setTimeout(() => {
+    carousel.appendChild(images[prevIndex]);
+    carousel.classList.remove("sliding-transition");
+    carousel.style.transform = "";
+    done = true;
+  }, 500);
+}
+
+// Gesture
+function handleGesture() {
+  if (touchendX < touchstartX) swipeLeft();
+  if (touchendX > touchstartX) swipeRight();
+}
+
+// init
+let done = true;
+
+// Swipe
+if (!!carousel) {
+  carousel.addEventListener('touchstart', function(event) {
+    touchstartX = event.screenX;
+  }, false);
+
+  carousel.addEventListener('touchend', function(event) {
+    touchendX = event.screenX;
+    handleGesture();
+  }, false);
+}
+
+
 if (!!leftArrow) {
-  let done = true;
   leftArrow.addEventListener("click", () => {
     if (done === false) return;
 
     done = false;
-    prevIndex = currentIndex;
-    currentIndex = (currentIndex - 1 + totalImages) % totalImages;
-
-    // Use previous image width
-    const index = prevIndex === 0 ? (images.length - 1) : (prevIndex - 1)
-    const w = images[index].getBoundingClientRect().width + gap;
-    // carousel.style.transform = `translateX(-${imageWidth}px)`;
-    carousel.style.transform = `translateX(-${w}px)`;
-    carousel.insertBefore(images[currentIndex], carousel.firstChild);
-
-    setTimeout(() => {
-      carousel.style.transform = "";
-      carousel.classList.add("sliding-transition");
-    }, 10);
-
-    setTimeout(() => {
-      carousel.classList.remove("sliding-transition");
-      done = true;
-    }, 490);
+    swipeLeft();
   });
-};
+}
 
 if (!!rightArrow) {
-  let done = true;
   rightArrow.addEventListener("click", () => {
     if (done === false) return;
 
     done = false;
-    carousel.classList.add("sliding-transition");
-
-    prevIndex = currentIndex;
-    currentIndex = (currentIndex + 1) % totalImages;
-
-    // Use current image width
-    const w = images[prevIndex].getBoundingClientRect().width + gap;
-
-    // carousel.style.transform = `translateX(-${imageWidth}px)`;
-    carousel.style.transform = `translateX(-${w}px)`;
-
-    setTimeout(() => {
-      carousel.appendChild(images[prevIndex]);
-      carousel.classList.remove("sliding-transition");
-      carousel.style.transform = "";
-      done = true;
-    }, 500);
+    swipeRight();
   });
-};
+}
